32e104d51683eecdadffbcbe409065c807f30c1d
Tiago Dinis <tiagodinis33@gmail.com>
2020-09-06 12:41:29 +0100

    Token migrado para um arquivo chamado token

--- src/main/java/org/enderman/Main.java
+++ src/main/java/org/enderman/Main.java
@@ -1,5 +1,9 @@
 package org.enderman;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.JDA;
@@ -8,7 +12,13 @@ import net.dv8tion.jda.api.JDABuilder;
 public class Main {
     public static void main(String[] args) {
 		JDA jda;
-		String token = "NjgyMTg0Nzg5MjU1NTIwMjkw.XlZUGA.w3CHsuI3vxsj8rrjzNAwlccgbyY";
+		String token = null;
+		try {
+			Scanner scanner = new Scanner(new FileReader("token"));
+			token = scanner.nextLine();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 		try {
 			jda = JDABuilder.createDefault(token).build();
 			jda.awaitReady();
